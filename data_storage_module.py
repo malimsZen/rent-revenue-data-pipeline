2b2a68b286fd36cb21317cedc346d5de399e7f83
malims <raphaelmalimsj@gmail.com>
2023-04-29 01:43:27 +0300

    Transform & Data Load to Data Warehouse

--- data_storage_module.py
+++ data_storage_module.py
@@ -1,52 +1,26 @@
-# Load the data from the staging table into the data warehouse table entities; property, tenant, time and rent_transaction
+import psycopg2 as psg
+import pandas as pd
 
-import psycopg2
-from faker import Faker
+#connect to postgres database
+conn = psg.connect(
+    database="postgres",
+    user="malims",
+    password="gn0m3t@mu",
+    host="localhost",
+    port="5432"
+)
 
-fake = Faker()
+#cursor object
+cursor = conn.cursor()
 
-def transform_load_data():
-    # Connect to the database
-    conn = psycopg2.connect(host='localhost', dbname='postgres', user='malims', password='gn0m3t@mu', port='5432')
-    cursor = conn.cursor()
+#execute query
+cursor.execute("SELECT * FROM staging")
 
-    # Define the SQL queries for transforming and loading data
-    transform_query = """
-        ALTER TABLE property ADD CONSTRAINT property_pk PRIMARY KEY (property_id);
-        ALTER TABLE time ADD CONSTRAINT time_pk PRIMARY KEY (time_id);
-        
-        INSERT INTO time (payment_date, year, quarter, month, day, day_of_week, day_of_year, week_of_year, month_name, quarter_name, year_month, year_quarter)
-        SELECT payment_date, EXTRACT(YEAR FROM payment_date), EXTRACT(QUARTER FROM payment_date), EXTRACT(MONTH FROM payment_date), 
-               EXTRACT(DAY FROM payment_date), EXTRACT(DOW FROM payment_date), EXTRACT(DOY FROM payment_date), 
-               EXTRACT(WEEK FROM payment_date), TO_CHAR(payment_date, 'Month'), 
-               CONCAT('Q', EXTRACT(QUARTER FROM payment_date), ' ', EXTRACT(YEAR FROM payment_date)), 
-               TO_CHAR(payment_date, 'YYYY-MM'), 
-               CONCAT(EXTRACT(YEAR FROM payment_date), ' Q', EXTRACT(QUARTER FROM payment_date))
-        FROM staging
-        ON CONFLICT (payment_date) DO NOTHING;
-        
-        INSERT INTO tenant (tenant_id, tenant_name, tenant_email, tenant_phone_number)
-        SELECT DISTINCT tenant_id, COALESCE(s.tenant_name, fake.name()), COALESCE(s.tenant_email, fake.email()), COALESCE(s.tenant_phone_number, fake.phone_number())
-        FROM staging s
-        LEFT JOIN tenant t ON s.tenant_id = t.tenant_id
-        ON CONFLICT (tenant_id) DO NOTHING;
-        
-        INSERT INTO property (property_id, property_name, property_address, property_type, number_of_units)
-        SELECT DISTINCT property_id, COALESCE(s.property_name, fake.company()), COALESCE(s.property_address, fake.address()), COALESCE(s.property_type, fake.word()), 
-               COALESCE(s.number_of_units, fake.random_int(min=1, max=10))
-        FROM staging s
-        LEFT JOIN property p ON s.property_id = p.property_id
-        ON CONFLICT (property_id) DO NOTHING;
-    """
+# transform data into pandas dataframe
+data = pd.DataFrame(cursor.fetchall())
 
-    # Execute the transformation and loading queries
-    cursor.execute(transform_query)
-    conn.commit()
+# load property table of postgres database using data from staging table. Generate values for the columns that are not in the staging table.
+# property attributes and data types (property_id INT NOT NULL,property_name VARCHAR(50) NOT NULL,property_address VARCHAR(50) NOT NULL,property_type VARCHAR(50) NOT NULL,number_of_units INT NOT NULL,PRIMARY KEY (property_id))
 
-    # Close the database connection
-    cursor.close()
-    conn.close()
 
-# Call the function to execute the transformation and loading
-transform_load_data()
 
