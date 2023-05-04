34f0c72dc9ef8b165fa950be7b87fc142f9f44ff
malims_zen <75028470+malimsZen@users.noreply.github.com>
2023-05-04 17:54:10 +0000

    rename of property_tenant lookup table

--- property_tenant_lookup_tables.py
+++ property_tenant_lookup_tables.py
@@ -1,5 +1,6 @@
 import random
 import psycopg2
+import pandas as pd
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
@@ -24,39 +25,21 @@ cur.execute("""
     FROM staging;
 """)
 
-# Generate and insert data into the tenant_lookup table
-cur.execute("""
-    INSERT INTO tenant_lookup (tenant_id, tenant_name, tenant_email, tenant_phone_number)
-    SELECT DISTINCT tenant_id, 
-           'Unknown' AS tenant_name, 
-           'Unknown' AS tenant_email, 
-           'Unknown' AS tenant_phone_number
-    FROM staging;
-""")
+# Create a dataframe from the property_lookup table and populate the missing attributes, property_id should be extracted from staging table in the database
+df = pd.read_sql_query("""
+   SELECT DISTINCT property_id,
+         'Unknown' AS property_name,
+        'Unknown' AS property_address,
+       'Unknown' AS property_type,
+      0 AS number_of_units
+ FROM staging;
+""", conn)
+df['property_name'] = random.choice(["Tsavo Divine", "Tsavo Rising", "Tsavo Stanley", "Sunset 2","Royal Suburbs","90 Degrees","Coral Bells"])
+df['property_address'] = "Unknown"
+df['property_type'] = random.choice(['Residential', 'Commercial', 'Industrial'])
+df['number_of_units'] = random.randint(1, 10)
+df.to_sql('property_lookup', conn, if_exists='replace', index=False)
 
-# Update the property_lookup table with generated fake data for missing attributes
-cur.execute("""
-    UPDATE property_lookup
-    SET property_name = %(property_name)s,
-        property_address = %(property_address)s,
-        property_type = %(property_type)s,
-        number_of_units = %(number_of_units)s
-    FROM (
-        SELECT DISTINCT property_id, 
-               %(property_name)s AS property_name, 
-               %(property_address)s AS property_address, 
-               %(property_type)s AS property_type, 
-               %(number_of_units)s AS number_of_units
-        FROM staging
-    ) AS data
-    WHERE property_lookup.property_id = data.property_id;
-""",
-{
-    "property_name": random.choice(["Tsavo Divine", "Tsavo Rising", "Tsavo Stanley", "Sunset 2","Royal Suburbs","90 Degrees","Coral Bells"]),
-    "property_address": "Unknown",
-    "property_type": random.choice(['Residential', 'Commercial', 'Industrial']),
-    "number_of_units": random.randint(1, 10)
-})
 
 # Update the tenant_lookup table with generated fake data for missing attributes
 cur.execute("""
--- temp_conn.session.sql
+++
@@ -1,2 +0,0 @@
-/* drop table staging */
-DROP TABLE staging;
